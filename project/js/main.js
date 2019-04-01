7ba6fc3e4e53ade0f854bb49c510ba8a505af2dd
leanne053 <46750182+leanne053@users.noreply.github.com>
2019-04-01 10:12:16 +0900

    project_up
    project_up

---
+++ project/js/main.js
@@ -0,0 +1,37 @@
+$(function(){
+    var memo = false;
+    var screen = document.getElementById("main");
+    var mouse = {
+        x:0,
+        y:0
+    }
+    var size = 20;
+    $('.select').on("click",function(){
+        Memo();
+
+    })
+    function Memo(x,y){
+    //properties
+    this.x=x;
+    this.y=y;
+    this.size=40;
+    this.div=document.createElement("div");
+    this.div.style.top=this.y+"px";
+    this.div.style.left=this.x+"px";
+    this.div.classList.add("memo");
+    //methods
+    this.draw=function(){
+        this.div.style.width=this.size+"px";
+        this.div.style.height=this.size+"px";
+        screen.appendChild(this.div);
+    }
+    }
+    //event Listeners
+    //mouse 위치에 div 생성하기
+    screen.addEventListener("click", function(e){
+        mouse.x=e.offsetX;
+        mouse.y=e.offsetY;
+        var m = new Memo(x,y);
+        m.draw();
+    })
+})
