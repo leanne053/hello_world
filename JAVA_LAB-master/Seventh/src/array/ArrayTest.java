02645fb8494bbd6474dbfde2f5d74dfa26a19701
leanne053 <46750182+leanne053@users.noreply.github.com>
2019-06-24 22:52:00 +0900

    7과 객체배열 및 객체배열 복사
    7과 객체배열 및 객체배열 복사

---
+++ JAVA_LAB-master/Seventh/src/array/ArrayTest.java
@@ -0,0 +1,18 @@
+package array;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		int[] num =new int[] {1,2,3,4,5,6,7,8,9,10};
+		int sum=0;
+		
+		for(int i=0;i<num.length;i++) {
+			sum+=num[i];
+			System.out.print(num[i]+", ");
+			
+		}
+		System.out.println();
+		System.out.println(sum);
+	}
+
+}
