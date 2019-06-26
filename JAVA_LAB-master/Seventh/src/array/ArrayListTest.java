4e59adb2b774d01acd02bb5c1e3f13470db89804
leanne053 <46750182+leanne053@users.noreply.github.com>
2019-06-26 23:59:29 +0900

    7과
    7과

---
+++ JAVA_LAB-master/Seventh/src/array/ArrayListTest.java
@@ -0,0 +1,26 @@
+package array;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		ArrayList<Book> library = new ArrayList();
+		
+		library.add(new Book("태백산맥", "조정래"));
+		library.add(new Book("데미안", "헤르만 헤세"));
+		library.add(new Book("어떻게 살 것인가", "유시민"));
+		
+		for(int i=0;i< library.size();i++) {
+			Book book = library.get(i);
+			book.showBookInfo();
+		}
+		System.out.println();
+		
+		System.out.println("===향상된 for문 사용===");
+		for(Book book : library) {
+			book.showBookInfo();
+		}
+	}
+
+}
